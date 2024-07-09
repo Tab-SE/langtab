7be8b81c55ded3c57cfb8521ecf998981061c63a
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-07-08 23:03:02 -0600

    VDS analyst agent

---
+++ agents/analyst.py
@@ -0,0 +1,15 @@
+from langchain_openai import ChatOpenAI
+from langchain.agents.agent_types import AgentType
+from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
+
+
+def vds_analyst(df):
+    agent = create_pandas_dataframe_agent(
+        ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0125"),
+        df,
+        verbose=True,
+        agent_type=AgentType.OPENAI_FUNCTIONS,
+        allow_dangerous_code=True,
+    )
+
+    return agent
