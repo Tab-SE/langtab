df68d4d202b1945572630dae9f4ece75ecf3364f
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-07-08 23:23:10 -0600

    defines langserve service

---
+++ modules/serve.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from langserve import add_routes
+
+async def langtab_agent(chain):
+    # define the server app
+    app = FastAPI(
+    title="LangTab Headless BI",
+    version="1.0",
+    description="An early demonstration of a LangChain agent querying Tableau's Headless BI service",
+    )
+    # use langserve to serve up the agent
+    add_routes(
+        app,
+        chain,
+        path="/chain",
+    )
+
+    return app
