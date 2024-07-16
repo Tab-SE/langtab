8d4e0629db011847ded75242e7e1c5361b9d96a8
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-07-16 17:16:26 -0600

    the agent is run and defined under agents/

--- agents/pandas.py
+++ agents/pandas.py
@@ -1,9 +1,17 @@
+import re
+import json
 from langchain_openai import ChatOpenAI
 from langchain.agents.agent_types import AgentType
 from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
 
+from modules import headless
 
-def pandas_agent(df):
+
+def pandas_agent(env_vars, chain, active_utterance):
+    output = chain.invoke(active_utterance)
+    payload = get_payload(output)
+    df = headless.query(env_vars, payload)
+
     agent = create_pandas_dataframe_agent(
         ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0125"),
         df,
@@ -13,3 +21,16 @@ def pandas_agent(df):
     )
 
     return agent
+
+
+def get_payload(output):
+    # print reasoning
+    print(output.split('JSON_payload')[0])
+    # parse LLM output and query headless BI
+    parsed_output = output.split('JSON_payload')[1]
+
+    match = re.search(r'{.*}', parsed_output, re.DOTALL)
+    if match:
+        json_string = match.group(0)
+        payload = json.loads(json_string)
+        return payload
