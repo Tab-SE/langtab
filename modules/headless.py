0cc32afb5188156907749d9d87c10fdd83265ee5
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-07-16 21:45:41 -0600

    clean up comments and prints

--- modules/headless.py
+++ modules/headless.py
@@ -21,19 +21,10 @@ def query(query):
 
     # Check if the request was successful (status code 200)
     if response.status_code == 200:
-        # Print the response data
-        #print("Response Data:")
-        #print(response.json())
         data = response.json()['data']
         # Create a pandas DataFrame from the JSON data
         df = pd.DataFrame(data)
-
-        # Display the first few rows of the DataFrame
-        #print("Table view of data from the public REST API:")
-        #print(df.head())
-        #print(df.all())
         return df
-        #display(df.head())
     else:
         print("Failed to fetch data from the API. Status code:", response.status_code)
         print(response.text)
