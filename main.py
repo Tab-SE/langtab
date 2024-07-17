a1f51abeda7d5a1616d0ab024cdfb69fa6487bc2
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-07-17 08:59:03 -0600

    moved utterance to main

--- main.py
+++ main.py
@@ -2,25 +2,29 @@ import argparse, uvicorn
 
 from dotenv import load_dotenv
 
-from modules import chain_config, utter, serve
+from modules import chain_config, serve
 
 # runs interactively in the terminal
 def run_interactive_mode(chain):
-    active_utterance = utter.get_utterance()
+    active_utterance = get_utterance()
     while active_utterance != 'stop':
         chain.invoke(active_utterance)
-        active_utterance = utter.get_utterance()
+        active_utterance = get_utterance()
 
 # runs as a service via Langserve
 def run_api_mode(chain, host, port=8000):
     app = serve.langtab_agent(chain)
     uvicorn.run(app, host=host, port=port)
 
+# prompts the user to continue asking questions
+def get_utterance():
+    query = input('What would you like to know about your data? Reply with "stop" if you are done. ')
+    return query
 
 def main():
     # environment variables available to current process and sub processes
     load_dotenv()
-    # runs the application in different modes: interactive & api
+    # runs the application in different modes: interactive (default) & api with optional arguments
     parser = argparse.ArgumentParser(description="Run the NLQ2VDS agent in interactive or API mode.")
     parser.add_argument("--mode", choices=["interactive", "api"], default="interactive", help="Run mode: interactive or api")
     parser.add_argument("--host", default="localhost", help="Host for API mode")
