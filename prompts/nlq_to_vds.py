9c61a5243012dc31776c8aca5d45ae832b1d8433
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-10-03 22:57:13 -0600

    prompt instructs query link to return results verbatim

--- prompts/nlq_to_vds.py
+++ prompts/nlq_to_vds.py
@@ -2,15 +2,16 @@ from prompts import vds_schema, few_shot
 
 
 instructions = """
-You are an expert at writing JSON payloads for Tableau’s VizQL Data Service (VDS) API.
-The VDS query is a JSON object that contains two fundamental components.
+You are an expert at writing API request bodies for Tableau’s HeadlessBI API.
+The HBI query is a JSON object that contains 2 fundamental components.
     1. columns [required] - an array of columns that define the desired output of the query
     2. filters [optional] - an array of filters to apply to the query. They can include fields that are not in the columns array.
 Your task is to retrieve data relevant to a user’s natural language query.
+
 A pandas AI Agent will be used to transform and analyze the data that your VDS query returns.
-Don't try to do too much with the json query. You will be successful if you bring back all the data that could help to answer the question, even if additional transformation and actions are needed.
-Query as much data as might be useful; it's ok if you pull in superfluous columns, but only use columns based on what is listed in the available_fields dictionary.
-DO NOT HALLUCINATE FIELD NAMES.
+Query as much data as might be useful; it's ok if you pull in superfluous columns,
+You will be successful if you bring back all the data that could help to answer the question, even if additional transformation and actions are needed.
+
 You can find the columnNames by checking the values of each key in the available_fields dictionary.
 The keys in the available_fields dictionary are the caption names for each column.
 The caption names are more likely to correspond to the user’s input, but you have to use the columnName when generating JSON.
@@ -19,6 +20,14 @@ Reasoning:
 
 JSON_payload:
 Make sure you use this structure so that it's simple to parse the output.
+Return query results verbatim so the pandas agent can analyze them.
+"""
+
+restrictions = """
+DO NOT HALLUCINATE FIELD NAMES.
+Don't try to do too much with the json query.
+Only use columns based on what is listed in the available_fields dictionary.
+Do not filter or reduce any data found in query results so the next link can determine future steps.
 """
 
 available_fields = [
@@ -44,6 +53,7 @@ available_fields = [
 
 prompt = {
     "instructions": instructions,
+    "restrictions": restrictions,
     "user_query": "",
     "available_fields": {},
     "data_model": [],
