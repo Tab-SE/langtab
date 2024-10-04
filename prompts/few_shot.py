b0d2b30a9fde871dc45dcd6e78d91f6b92d6269b
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-10-03 20:22:09 -0600

    few shots for superstore

--- prompts/few_shot.py
+++ prompts/few_shot.py
@@ -1,34 +1,32 @@
 examples = {
-    "example1": {
-        "query": "",
-        "JSON": ""
-    },
-    "example2": {
-        "query": "",
-        "JSON": ""
-    },
-    "example3": {
-        "query": "",
-        "JSON": ""
-    },
-    "example4": {
-        "query": "",
-        "JSON": ""
-    },
-    "example5": {
-        "query": "",
-        "JSON": ""
-    },
-    "example6": {
-        "query": "",
-        "JSON": ""
-    },
-    "example7": {
-        "query": "",
-        "JSON": ""
-    },
-    "example8": {
-        "query": "",
-        "JSON": ""
-    },
+    "superstore": {
+        "columns": {},
+        "filters": {},
+        "calculations": {},
+        "example1": {
+        "query": "Show me sales by segment",
+        "JSON": {
+            "columns": [
+                {"columnName": "Segment"},
+                {"columnName": "Sales", "function": "SUM", "maxDecimalPlaces": 2}
+            ]
+        }
+        },
+        "example2": {
+            "query": "Top selling sub-categories with a minimum of $200,000",
+            "JSON": {
+                "columns": [
+                    {"columnName": "Sub-Category"},
+                    {"columnName": "Sales", "function": "SUM", "sortPriority": 1, "sortDirection": "DESC"}
+                ],
+                "filters": [
+                    {"columnName": "Sales", "filterType": "QUANTITATIVE", "quantitativeFilterType": "MIN", "min": 200000}
+                ]
+            }
+        },
+        "example3": {
+            "query": "",
+            "JSON": ""
+        }
+    }
 }
